f2d4c623f1f3b383ce70af9eb76cce7977d14a0c
skdding3 <skdding3@gmail.com>
2022-08-22 08:08:55 +0900

    axios prac create

---
+++ src/Users.jsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+
+
+function Users() {
+    const [users, setUsers] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                setUsers(null)
+                setError(null)
+                setLoading(true)
+                const response = await axios.get(
+                    "https://jsonplaceholder.typicode.com/users"
+                )
+
+                console.log(response)
+
+                setUsers()
+            } catch ()
+        }
+        fetchUser()
+    },[]);
+
+    if (!users) {
+        return <p>로딩중 ..</p>;
+    }
+
+    return (
+        <ul>
+            {users.map((users) => (
+                <li key={user.id}>
+                    {user.username}---({user.name})
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default Users;
